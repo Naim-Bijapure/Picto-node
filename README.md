7c48da5a23ea5c393db0f2c5eccf8bca174eb2a4
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-04-12 18:42:03 +0530

    initial comnite

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Pictionary #
+### Pictionary app written in [Node.js ](https://nodejs.org/en/)and Socket.io ###
+##  How to run Pictionary
+To get node_modules directory type in terminal:
+
+	npm install
+Then: 
+
+	npm start
+
+After that, browse localhost:3000 to play a game,
+
+... or try live demo
+[HERE](https://pictionary-wlbptxpjpx.now.sh/ "Play Pictionary") 
+[**!!! NOTICE !!!** Canvas element works better on higher resolutions, low resolutions are not suported yet] 
+##  To do
+ - RWD
---
+++ server/server.js
@@ -0,0 +1,197 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const {Users} = require('./utils/users');
+const {stringValidation} = require('./utils/validation');
+const {Words} = require('./utils/words');
+const {generateMessage} = require('./utils/message');
+const {TimeLeft} = require('./utils/time');
+const {Img} = require('./utils/img'); 
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+app.use(express.static(publicPath));
+
+let users = new Users();
+let words = new Words();
+
+let timeLeft;
+let lastetIMG;
+
+let word = words.getRandomWord();
+
+io.on('connection', (socket)=>{
+    
+    socket.on('joinGame', (userName, callback)=>{
+        if(!stringValidation(userName)){
+            return callback('User name is required');
+        }
+
+        if(!users.users.length){
+            users.addUser(socket.id, userName, true);
+            socket.join('draw');
+            socket.emit("drawer", word.name);
+            timeLeft = new TimeLeft((time)=>{
+                if(time === 0){
+                  io.in('draw').emit('stopCanvas');
+                  let newDrawer = switchPlayers();
+                  io.emit('serverMessage', generateMessage(`Nobody`, ` guessed the '${word.name}'. ${newDrawer.name} is now drawing`));
+                  
+                  wordScoreEmits();
+                }
+                io.emit('timeLeft', time);
+              });
+              timeLeft.startCountDown();
+
+        }else{
+            users.addUser(socket.id, userName, false);
+            socket.join('guess');
+            socket.emit('clearCanvas');
+            socket.emit('drawing', lastetIMG);
+            socket.emit('guess', word.category);
+        }
+
+        socket.emit('serverMessage', generateMessage('Hi,', ` let's play a game`));
+        socket.broadcast.emit('serverMessage', generateMessage(`${userName} `, `has joined a game`));
+        io.emit('scoreBoard', users.users);
+
+    });
+
+    socket.on('message', (msg)=>{
+        if(!stringValidation(msg)) 
+             return false;
+
+        let user = users.getUser(socket.id);
+        if(word.name.toLowerCase() === msg.toLowerCase()){
+            io.in('draw').emit('stopCanvas');
+            let drawer = users.getDrawer();
+            users.addScore(socket.id);
+            users.addScore(drawer.id);
+            io.emit('chatWindow', generateMessage(user.name, msg));
+            io.emit('serverMessage', generateMessage(`${user.name} `, `has guessed the word : ${word.name}`));
+            switchPlayers(user);
+            wordScoreEmits();
+            timeLeft.resetTime();
+        }else{
+            io.emit('chatWindow', generateMessage(user.name, msg));
+
+        }
+
+    }); 
+    socket.on('drawing', (pic)=>{
+        if(!img.start)
+            img.startInterval();
+
+        img.addPic(pic);
+    });
+
+    let img = new Img((pic)=>{
+        io.in("guess").emit("drawing", pic);
+        lastetIMG = pic;
+    });
+    
+    socket.on('clear', ()=>{
+        io.emit('clearCanvas');
+    })
+
+    socket.on('disconnect', ()=>{
+        let user = users.removeUser(socket.id);
+        // jezeli last user opuscił gre,  user == undefined
+        if(user){
+            // jezeli user byl rysownikiem
+            if(user.canDraw){
+                let newDrawer = switchPlayers()
+                word = words.getRandomWord();
+                io.emit('clearCanvas');
+                io.emit('scoreBoard', users.users); 
+                timeLeft.resetTime();
+                if(!word){
+                    return gameover();
+                }
+                io.in('draw').emit('drawer', word.name);
+                socket.broadcast.emit('serverMessage', generateMessage(`Drawer`, ` has left the game. ${newDrawer.name} is now drawing`));    
+            }else{
+            socket.broadcast.emit('serverMessage', generateMessage(`${user.name} `, `has left the game`));
+            }
+        }
+        if(!users.users.length){
+            if(timeLeft){
+             timeLeft.stopInterval();
+             timeLeft.resetTime();
+            }
+        }   
+    })
+
+    function switchPlayers(user){
+        if(!users.users.length){
+            gameover();
+            return false
+        }
+        let drawer = users.getDrawer();
+
+        // Jezeli osoba rysująca opusciła gre, drawer == undefined, nalezy wylosować nowego rysującego
+        if(!drawer){
+            let newDrawer = users.pickRandomGuesser();
+            let newDrawerSocket = io.sockets.connected[newDrawer.id];
+            newDrawerSocket.leave('guess');
+            newDrawerSocket.join('draw');
+            newDrawer.canDraw = true;
+            timeLeft.resetTime();
+            return newDrawer;
+        }
+        if(users.users.length == 1){
+            timeLeft.resetTime();
+            return drawer;
+        }
+        let newDrawer = users.pickRandomGuesser();
+        
+        let drawerSocket = io.sockets.connected[drawer.id];
+        drawerSocket.leave('draw');
+        drawerSocket.join('guess');
+        drawer.canDraw = false;
+        // User ktory odgadł hasło, bedzie teraz rysować.
+        if(user){
+            let newDrawerSocket = io.sockets.connected[user.id];
+            newDrawerSocket.leave('guess');
+            newDrawerSocket.join('draw');
+            user.canDraw = true;
+            timeLeft.resetTime();
+            return user;
+        }
+        let newDrawerSocket = io.sockets.connected[newDrawer.id];
+        newDrawerSocket.leave('guess');
+        newDrawerSocket.join('draw');
+        newDrawer.canDraw = true;
+        timeLeft.resetTime();
+        return newDrawer;
+      }
+
+    function wordScoreEmits(){
+        word = words.getRandomWord();
+        
+        io.emit('scoreBoard', users.users);
+        io.emit('clearCanvas');
+        if(!word){
+            return gameover();
+        }
+        io.in("draw").emit('drawer', word.name);
+        io.in("guess").emit('guess', word.category);
+      }
+
+      function gameover () {
+        timeLeft.stopInterval();
+        if(users.users.length > 0){
+            io.emit('serverMessage', generateMessage(`Game Over`, `${users.users[0].name} is the winner`)); 
+        }
+      }
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+  })
+  
